931dfbace0e4f22cc7c059059b8ecb07c9f53b67
uzumakinarutodaatebayo <128773340+uzumakinarutodaatebayo@users.noreply.github.com>
2023-03-26 21:08:43 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "14157322"))
+    API_HASH = os.environ.get("API_HASH", "9271a3e8d68ea664d83964953b8de084")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6047099925:AAHSD6HnuwoUA_l7FlSGHUQx_zitMjBDvh8")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "movies_zilla_bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
